f63cb763ebb62ee4b81eb4e2ea97a0ca1efc3cce
Andreas Nold <andreasnold05@gmail.com>
2020-10-13 13:50:30 +0200

    changed about

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
-export default function About() {
+export default function about() {
   return (
     <Layout title="About me">
       <Head>
